df6113d85eb7f6a893e2deb29b88db73df5e72b7
Greg Conant <g.conant@elsevier.com>
2016-10-10 16:11:06 -0400

    gc - renaming an annotation

--- GitDemo/Controllers/AnnotationsController.cs
+++ GitDemo/Controllers/AnnotationsController.cs
@@ -11,7 +11,7 @@ namespace GitDemo.Controllers
         // GET: Annotations
         public ActionResult Index()
         {
-            var annotationsToReturn = new List<string> {"Annotation 1", "Annotation 2", "Annotation 31"};
+            var annotationsToReturn = new List<string> {"Annotation 1", "Annotation 2", "Annotation 3"};
             return View(annotationsToReturn);
         }
 
